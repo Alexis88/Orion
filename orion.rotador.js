9bada1ac4bf446c5d7f8cd8965113e3ea39d1ce8
Alexis López Espinoza <Alexis88@users.noreply.github.com>
2014-03-02 14:04:04 -0500

    Update orion.rotador.js

--- orion.rotador.js
+++ orion.rotador.js
@@ -17,18 +17,22 @@ var Orion = function(identificador){
                 this.objeto = document.getElementById(identificador.substr(1));
                 break;
             case ".": //Tomo al elemento por su Clase
-                this.objeto = document.getElementsByClassName ? document.getElementsByClassName(identificador.substr(1))[0] : document.querySelectorAll ? document.querySelectorAll(identificador)[0] : function(identificador){
+                this.objeto = document.getElementsByClassName ? document.getElementsByClassName(identificador.substr(1)) : document.querySelectorAll ? document.querySelectorAll(identificador) : function(identificador){
                     var todos = document.getElementsByTagName("*"),
-                        todosTotal = todos.length;
+                        todosTotal = todos.length,
+                        obj = [];
                     for (i = 0; i < todosTotal; i++)
                         if (todos[i].className == identificador.substr(1))
-                            return todos[i];
+                            obj.push(todos[i]);
+                    return obj;
                 };
                 break;        
             default: //Tomo al elemento por su etiqueta
-                this.objeto = document.getElementsByTagName(identificador)[0];
+                this.objeto = document.getElementsByTagName(identificador);
                 break;
         }
+
+        this.total = this.objeto.length;
         
         return this;
     }
@@ -36,52 +40,58 @@ var Orion = function(identificador){
  
 Orion.prototype = {
     rotador: function(json){
-        /*
-            Tomo a todas las imágenes anexadas, les aplico estilos
-            y las añado al elemento de referencia
-        */
-        for (var i in json.imagenes){
-            var img = document.createElement("img");
-            img.src = json.imagenes[i];
-            img.style.position = "absolute";
-            img.style.opacity = 0;
-            img.style.width = json.ancho;
-            img.style.height = json.alto;
-            img.style.transition = ".75s";
-            this.objeto.appendChild(img);
-        }
-        
-        var velocidad = 1000; //Por defecto, el tiempo de transición será de 1 segundo
-        
-        //Control de velocidad de transición
-        if (json.velocidad)
-            switch (json.velocidad) {
-                case "lento":
-                    velocidad = 5000;
-                    break;
-                case "normal":
-                    velocidad = 3000;
-                    break;
-                case "rapido":
-                    velocidad = 1500;
-                    break;
+        var aplicar = function(elObjeto){
+            /*
+                Tomo a todas las imágenes anexadas, les aplico estilos
+                y las añado al elemento de referencia
+            */
+            for (var j in json.imagenes){
+                var img = document.createElement("img");
+                img.src = json.imagenes[j];
+                img.style.position = "absolute";
+                img.style.opacity = 0;
+                img.style.width = json.ancho;
+                img.style.height = json.alto;
+                img.style.transition = ".75s";
+                this.appendChild(img);
             }
-        
-        var imagenes = this.objeto.getElementsByTagName("img"), //Tomo a todas las imágenes anexadas
-            total = imagenes.length, //Calculo el total de imágenes anexadas
-            contador = 0; //El contador que nos permitirá rotar a las imágenes
             
-        //Mostramos a la primera imagen
-        imagenes[contador].style.opacity = 1;
-        
-        var show = function(){
-            imagenes[contador].style.opacity = 0; //Oculto a la imagen actual
-            contador = contador == total - 1 ? 0 : ++contador; //Actualizo el valor del contador
-            imagenes[contador].style.opacity = 1; //Muestro a la siguiente imagen en la secuencia
-        };
-        
-        setInterval(show, velocidad); //El plugin se ejecutará cada "velocidad" segundos de manera indefinida
-        
-        return this; //Retornamos al objeto
+            var velocidad = 1000; //Por defecto, el tiempo de transición será de 1 segundo
+            
+            //Control de velocidad de transición
+            if (json.velocidad)
+                switch (json.velocidad) {
+                    case "lento":
+                        velocidad = 5000;
+                        break;
+                    case "normal":
+                        velocidad = 3000;
+                        break;
+                    case "rapido":
+                        velocidad = 1500;
+                        break;
+                }
+            
+            var imagenes = this.getElementsByTagName("img"), //Tomo a todas las imágenes anexadas
+                total = imagenes.length, //Calculo el total de imágenes anexadas
+                contador = 0; //El contador que nos permitirá rotar a las imágenes
+                
+            //Mostramos a la primera imagen
+            imagenes[contador].style.opacity = 1;
+            
+            var show = function(){
+                imagenes[contador].style.opacity = 0; //Oculto a la imagen actual
+                contador = contador == total - 1 ? 0 : ++contador; //Actualizo el valor del contador
+                imagenes[contador].style.opacity = 1; //Muestro a la siguiente imagen en la secuencia
+            };
+            
+            setInterval(show, velocidad); //El plugin se ejecutará cada "velocidad" segundos de manera indefinida
+        }
+
+        //Aplico el plugin en cada elemento
+        for (i = 0; i < this.total; i++)
+            aplicar.apply(this.objeto[i]);
+
+        return this; //Retornamos el objeto
     }
 };
